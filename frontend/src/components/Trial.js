db431b4695c5e3de1e904ab77c433ef4b1023e1c
ngecu <devngecu@gmail.com>
2023-03-24 16:02:19 +0300

    clients variables

--- frontend/src/components/Trial.js
+++ frontend/src/components/Trial.js
@@ -129,7 +129,7 @@ export default function Trial({ session }) {
           name:prompt,
           link,
           user_id:session.user.id,
-          timestamp:new Date().valueOf()
+          
         })
         .single()
         
